570c7c8a5016c4d365fa8d965f64dc0474478dd0
Ziel <frans_421_12@hotmail.com>
2022-12-18 19:42:06 -0500

    feat: Diseño Navbar

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize.js'
 
 const app = createApp(App)
 
